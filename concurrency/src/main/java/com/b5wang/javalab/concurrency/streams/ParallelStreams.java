97721b0eaee946731603d8ae119049fc41f43b83
b5wang <neo.b5wang@gmail.com>
2020-07-21 18:38:58 +0800

    parallel streams
    parallel streams

---
+++ concurrency/src/main/java/com/b5wang/javalab/concurrency/streams/ParallelStreams.java
@@ -0,0 +1,17 @@
+package com.b5wang.javalab.concurrency.streams;
+
+import java.util.Arrays;
+import java.util.stream.Stream;
+
+public class ParallelStreams {
+
+    public static void main(String[] args){
+        // Get parallel stream from stream
+        Stream<Integer> stream = Arrays.asList(1,2,3,4,5,6).stream();
+        Stream<Integer> parallelStream = stream.parallel();
+
+        // Get parallel stream from collection
+        Stream<Integer> parallelStream2 = Arrays.asList(1,2,3,4,5,6).parallelStream();
+    }
+
+}
