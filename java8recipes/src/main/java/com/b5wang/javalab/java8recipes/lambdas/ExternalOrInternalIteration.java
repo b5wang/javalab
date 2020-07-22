4320ffd2538fbb1089dc26f0a5f9378fd46b695c
b5wang <neo.b5wang@gmail.com>
2020-07-22 13:53:02 +0800

    create java8recipes project
    For Java 8+ recipes

---
+++ java8recipes/src/main/java/com/b5wang/javalab/java8recipes/lambdas/ExternalOrInternalIteration.java
@@ -0,0 +1,15 @@
+package com.b5wang.javalab.java8recipes.lambdas;
+
+import java.util.Arrays;
+import java.util.List;
+
+public class ExternalOrInternalIteration {
+
+    public static void main(String[] args){
+        List<Integer> list = Arrays.asList(1,2,3,4,5,6,7,8,9,10);
+
+        // External iteration
+
+    }
+
+}
