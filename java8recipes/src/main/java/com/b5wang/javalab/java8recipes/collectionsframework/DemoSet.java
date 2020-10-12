c209e0cbb7249b20af5f2133fcafad7bc1c83b0c
b5wang <neo.b5wang@gmail.com>
2020-10-12 23:27:15 +0800

    equals wrong implementation
    equals wrong implementation

---
+++ java8recipes/src/main/java/com/b5wang/javalab/java8recipes/collectionsframework/DemoSet.java
@@ -0,0 +1,19 @@
+package com.b5wang.javalab.java8recipes.collectionsframework;
+
+public class DemoSet {
+
+    /**
+     * a collection that cannot contain duplicate elements.
+     * The Set interface contains only methods inherited from Collection and adds the restriction
+     * that duplicate elements are prohibited. Set also adds a stronger contract on the behavior
+     * of the equals and hashCode operations, allowing Set instances to be compared meaningfully
+     * even if their implementation types differ.
+     * Two Set instances are equal if they contain the same elements.
+     *
+     * Set implementations: HashSet, TreeSet, and LinkedHashSet. 
+     * */
+    public static void main(String[] args){
+
+    }
+
+}
