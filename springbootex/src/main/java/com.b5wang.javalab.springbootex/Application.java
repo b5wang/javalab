1fbe6c0c21bbc7b3064617e8cea16787b279566a
B5WANG <b5wang@gmail.com>
2020-12-22 23:41:18 +0800

    spring async
    spring async

--- springbootex/src/main/java/com.b5wang.javalab.springbootex/Application.java
+++ springbootex/src/main/java/com.b5wang.javalab.springbootex/Application.java
@@ -5,10 +5,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableAsync;
+import org.springframework.scheduling.concurrent.ThreadPoolTaskExecutor;
 
 import java.util.Arrays;
+import java.util.concurrent.Executor;
 
 @SpringBootApplication
+@EnableAsync
 public class Application {
 
     public static void main(String[] args) {
@@ -20,17 +24,28 @@ public class Application {
      * 两个接口的作用相同，不同的是run方法接收的参数，
      * Ref: https://www.jianshu.com/p/de7b0e124248
      * */
-    //@Bean
+    @Bean
     public CommandLineRunner commandLineRunner(ApplicationContext ctx) {
         return args -> {
 
-            System.out.println("Let's inspect the beans provided by Spring Boot:");
+            System.out.println("Chance to do something just after application started");
 
-            String[] beanNames = ctx.getBeanDefinitionNames();
-            Arrays.sort(beanNames);
-            for (String beanName : beanNames) {
-                System.out.println(beanName);
-            }
+//            String[] beanNames = ctx.getBeanDefinitionNames();
+//            Arrays.sort(beanNames);
+//            for (String beanName : beanNames) {
+//                System.out.println(beanName);
+//            }
         };
+    }
+
+    @Bean
+    public Executor taskExecutor() {
+        ThreadPoolTaskExecutor executor = new ThreadPoolTaskExecutor();
+        executor.setCorePoolSize(10);
+        executor.setMaxPoolSize(20);
+        executor.setQueueCapacity(500);
+        executor.setThreadNamePrefix("async-");
+        executor.initialize();
+        return executor;
     }
 }
